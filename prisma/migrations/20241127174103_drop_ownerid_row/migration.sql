91782df8f3f5f1c81061c56318629f0c694a0d56
ELORCHI <elorchie@gmail.com>
2024-11-27 18:44:19 +0100

    remove owner id from guild

---
+++ prisma/migrations/20241127174103_drop_ownerid_row/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ownerId` on the `Guild` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Guild" DROP COLUMN "ownerId";
