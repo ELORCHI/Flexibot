2c057fda4e80261543fdf2f1967f87a96b47005d
ELORCHI <elorchie@gmail.com>
2024-11-29 04:05:29 +0100

    updare client type name

--- src/db/commandStatus.ts
+++ src/db/commandStatus.ts
@@ -14,48 +14,48 @@ export const getCommandStatus = async (guildId: string, command: string) => {
 };
 
 // Function to create or update command status for a guild/channel
-export const setCommandStatus = async (guildId: string, command: string, enabled: boolean, channelId?: string) => {
-  try {
-    const commandStatus = await prisma.commandStatus.upsert({
-      where: {
-        guildId_channelId_command: {
-          guildId,
-          command,
-          channelId: channelId ?? "",
-        },
-      },
-      create: {
-        guildId,
-        command,
-        enabled,
-        channelId: channelId ?? undefined,
-      },
-      update: {
-        enabled,
-      },
-    });
-    return commandStatus;
-  } catch (error) {
-    console.error("Error setting command status:", error);
-    throw error;
-  }
-};
+// export const setCommandStatus = async (guildId: string, command: string, enabled: boolean, channelId?: string) => {
+//   try {
+//     const commandStatus = await prisma.commandStatus.upsert({
+//       where: {
+//         guildId_channelId_command: {
+//           guildId,
+//           command,
+//           channelId: channelId ?? "",
+//         },
+//       },
+//       create: {
+//         guildId,
+//         command,
+//         enabled,
+//         channelId: channelId ?? undefined,
+//       },
+//       update: {
+//         enabled,
+//       },
+//     });
+//     return commandStatus;
+//   } catch (error) {
+//     console.error("Error setting command status:", error);
+//     throw error;
+//   }
+// };
 
-// Function to delete a command status by guild and command name
-export const deleteCommandStatus = async (guildId: string, command: string, channelId?: string) => {
-  try {
-    const deletedCommandStatus = await prisma.commandStatus.delete({
-      where: {
-        guildId_channelId_command: {
-          guildId,
-          command,
-          channelId: channelId ?? "",
-        },
-      },
-    });
-    return deletedCommandStatus;
-  } catch (error) {
-    console.error("Error deleting command status:", error);
-    throw error;
-  }
-};
+// // Function to delete a command status by guild and command name
+// export const deleteCommandStatus = async (guildId: string, command: string, channelId?: string) => {
+//   try {
+//     const deletedCommandStatus = await prisma.commandStatus.delete({
+//       where: {
+//         guildId_channelId_command: {
+//           guildId,
+//           command,
+//           channelId: channelId ?? "",
+//         },
+//       },
+//     });
+//     return deletedCommandStatus;
+//   } catch (error) {
+//     console.error("Error deleting command status:", error);
+//     throw error;
+//   }
+// };
--- src/types/discord.ts
+++ src/types/discord.ts
@@ -1,10 +1,15 @@
-import { Client, Collection, ApplicationCommand, ClientOptions } from 'discord.js';
+import {
+  Client,
+  Collection,
+  ApplicationCommand,
+  ClientOptions,
+} from "discord.js";
 
-export default class ClientCommands extends Client {
-    public commands: Collection<string, ApplicationCommand>;
+export default class ClientWithCommands extends Client {
+  public commands: Collection<string, ApplicationCommand>;
 
-    constructor(options: ClientOptions) {
-        super(options);
-        this.commands = new Collection<string, ApplicationCommand>();
-    }
+  constructor(options: ClientOptions) {
+    super(options);
+    this.commands = new Collection<string, ApplicationCommand>();
+  }
 }
