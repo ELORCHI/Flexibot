db3db843607e7d10a8fc10f2b145456cacc551f6
ELORCHI <elorchie@gmail.com>
2024-11-29 07:43:51 +0100

    set the guild as enabled after adding the bot to the server

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,11 +1,18 @@
 import { Guild } from "discord.js";
 import { registerGuildCommands } from "../utils/registerCommands";
 import ClientWithCommands from "../types/discord";
+import { prisma } from "../db/prismaClient";
 
 export default {
   name: "guildCreate",
   async execute(guild: Guild, client: ClientWithCommands) {
     await registerGuildCommands(client, guild.id);
+    await prisma.guild.update({
+      where: { id: guild.id },
+      data: {
+        enabled: true,
+      },
+    });
     console.log(`Bot joined a new guild: ${guild.name} (ID: ${guild.id})`);
   },
 };
