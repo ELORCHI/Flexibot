9da38b6ad78a18b74c0bbf8f62a4ec7da5166775
ELORCHI <elorchie@gmail.com>
2024-11-30 07:30:04 +0100

    fix guildDelete event handler

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -1,9 +1,9 @@
 import { Guild } from "discord.js";
-import { Interaction } from "discord.js";
 import { deleteGuildWithAllRelatedRecords } from "../db/guild";
+import ClientWithCommands from "../types/discord";
 export default {
   name: "guildDelete",
-  async execute(interaction: Interaction, guild: Guild) {
+  async execute(guild: Guild, client: ClientWithCommands) {
     console.log(`The bot was removed from: ${guild.name} (ID: ${guild.id})`);
     await deleteGuildWithAllRelatedRecords(guild.id);
   },
