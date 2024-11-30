db77b74e4ca52eacaf351415f0c88a0ff1399c0e
ELORCHI <elorchie@gmail.com>
2024-11-30 07:35:04 +0100

    update slash Command interaction to check for command config before executing

--- src/events/slashCommandInteraction.ts
+++ src/events/slashCommandInteraction.ts
@@ -1,6 +1,10 @@
 import { Interaction, ChatInputCommandInteraction } from "discord.js";
 import ClientWithCommands from "../types/discord";
 import { Command } from "../types/command";
+import { PrismaClient } from "@prisma/client";
+import { checkCommandPermissions } from "../utils/CheckCommand";
+
+const prisma = new PrismaClient();
 
 export default {
   name: "interactionCreate",
@@ -12,11 +16,28 @@ export default {
     ) as unknown as Command;
     if (!command) {
       return await interaction.reply({
-        content: "There was an error while executing this command!",
+        content: "Command not fount",
         ephemeral: true,
       });
     }
+    console.log({ interaction });
     try {
+      // Check command permissions
+      const permissionCheck = await checkCommandPermissions(
+        interaction as ChatInputCommandInteraction,
+        prisma
+      );
+
+      if (!permissionCheck.allowed) {
+        return await interaction.reply({
+          content:
+            permissionCheck.reason ||
+            "You are not allowed to use this command.",
+          ephemeral: true,
+        });
+      }
+
+      // Execute the command if permissions check passes
       await command.execute(interaction as ChatInputCommandInteraction);
     } catch (error) {
       console.error(error);
